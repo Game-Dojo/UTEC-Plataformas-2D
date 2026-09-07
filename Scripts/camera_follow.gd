074b6e7a5b43c1e2fe0df4bb3fa0ed35aaebb185
Fede <fedealvareztorres@gmail.com>
2026-09-07 15:54:21 -0300

    Last changes

--- Scripts/camera_follow.gd
+++ Scripts/camera_follow.gd
@@ -1,9 +1,10 @@
 extends Camera2D
 
 @onready var player: CharacterBody2D = $"../Player"
+@export var camera_speed := 0.3
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	global_position = lerp(global_position, player.global_position, delta * camera_speed)
